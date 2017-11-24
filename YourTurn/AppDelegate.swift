fb23f50cf0245719ae71aba6c4de19c3b6219971
Josh Caswell <josh@scylla>
2017-11-24 18:24:59 -0500

    Initial commit.

---
+++ YourTurn/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate : UIResponder, UIApplicationDelegate
+{
+    typealias LaunchOptions = [UIApplicationLaunchOptionsKey : Any]
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions options: LaunchOptions?)
+        -> Bool
+    {
+        return true
+    }
+}
---
+++ YourTurn/PlayerSwitchViewController.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+/** PlayerSwitchViewController documentation */
+class PlayerSwitchViewController : UIViewController {
+
+}
